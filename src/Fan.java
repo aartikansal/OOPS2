06eddce9e22f8305e9c1ab9f87857dcd945b75b4
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-18 13:15:37 -0500

    runner, obj, state added print current state--string.format added

--- src/Fan.java
+++ src/Fan.java
@@ -1,8 +1,23 @@
 public class Fan {
+
     //State
     private String make;
     private double radius;
     private String color;
     private boolean isOn;
     private byte speed;
+
+    //creation---constructor
+    public Fan(String make, double radius, String color){
+        this.make = make;
+        this.radius = radius;
+        this.color = color;
+
+    }
+    //PRINT THE STATE
+    public String toString(){
+       return String.format( "make - %s, radius- %f, color- %s, isOn- %b,speed-%d,",
+               make, radius, color, isOn, speed);
+    }
+
 }
--- src/FanRunner.java
+++ src/FanRunner.java
@@ -1,5 +1,6 @@
 public class FanRunner{
     public static void main(String[] args) {
-        Fan fan = new Fan();
+        Fan fan = new Fan("Manufacturer 1",0.34567, "GREEN");
+        System.out.println(fan);
     }
 }
