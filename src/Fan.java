07acd04bdac201af839cebdf752f740208d4e545
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-18 13:04:33 -0500

    Initial commit

---
+++ src/Fan.java
@@ -0,0 +1,7 @@
+public class Fan {
+    private String make;
+    private double radius;
+    private String color;
+    private boolean isOn;
+    private byte speed;
+}
---
+++ src/FanRunner.java
@@ -0,0 +1,5 @@
+public class FanRunner{
+    public static void main(String[] args) {
+        Fan fan = new Fan();
+    }
+}
