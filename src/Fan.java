c95263c94dada620ff758d95ff154cb9b7747135
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-18 13:25:32 -0500

    runner, obj, state added print current state--string.format added speed method added, switch on/off

--- src/Fan.java
+++ src/Fan.java
@@ -15,6 +15,20 @@ public class Fan {
 
     }
     //PRINT THE STATE
+    // isOn
+    public void switchOn(){
+        this.isOn = true;
+        setSpeed((byte)5);
+
+    }
+    public void switchOff() {
+        this.isOn = false;
+        setSpeed((byte)0);
+    }
+    public void setSpeed(byte speed){
+        this.speed = speed;
+    }
+
     public String toString(){
        return String.format( "make - %s, radius- %f, color- %s, isOn- %b,speed-%d,",
                make, radius, color, isOn, speed);
